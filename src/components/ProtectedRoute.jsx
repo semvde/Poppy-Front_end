dd6e16a73929aa69d7f98dc77faa0f1441f4f9de
Semmetje <semvandenende@gmail.com>
2026-03-11 19:11:07 +0100

    Added ProtectedRoute component for user authentication and onboarding flow.

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,44 @@
+import {useEffect, useState} from "react";
+import {Navigate, useLocation} from "react-router";
+import {fetchAPI} from "../services/Fetch.js";
+import AppLayout from "../layouts/AppLayout.jsx";
+
+export default function ProtectedRoute({children}) {
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
+    const location = useLocation();
+
+    useEffect(() => {
+        const checkLogin = async () => {
+            setLoading(true);
+
+            const res = await fetchAPI("/auth/me");
+
+            if (res?.id) {
+                setUser(res);
+            } else {
+                setUser(null);
+            }
+
+            setLoading(false);
+        };
+
+        checkLogin();
+    }, [location.pathname]);
+
+    if (loading) return <AppLayout/>;
+
+    if (!user) {
+        return <Navigate to="/login" replace/>;
+    }
+
+    if (!user.hasCompletedOnboarding && location.pathname !== "/app/onboarding") {
+        return <Navigate to="/app/onboarding" replace/>;
+    }
+
+    if (user.hasCompletedOnboarding && location.pathname === "/app/onboarding") {
+        return <Navigate to="/app" replace/>;
+    }
+
+    return children;
+}
