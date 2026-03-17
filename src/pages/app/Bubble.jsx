9beb4c0ffc251c47d2800f6906dcb46cd932f4e3
eline <1096534@hr.nl>
2026-03-17 10:51:07 +0100

    ADD user data to the bubble

--- src/pages/app/Bubble.jsx
+++ src/pages/app/Bubble.jsx
@@ -1,33 +1,68 @@
-function Bubble() {
-    const bubbleItems = [
-        {
-            "name": "EDM",
-            "listen_duration": 30
-        },
-        {
-            "name": "House",
-            "listen_duration": 25
-        },
-        {
-            "name": "Pop",
-            "listen_duration": 20
-        }
-    ];
+import {fetchAPI} from "../../services/Fetch.js";
+import {useEffect, useState} from "react";
 
-    const bubbleNewItems = [
-        {
-            "name": "Rap",
-            "listen_duration": 10
-        },
-        {
-            "name": "Trance",
-            "listen_duration": 5
-        },
-        {
-            "name": "DnB",
-            "listen_duration": 2
+function Bubble() {
+
+    const [bubbleItems, setBubbleItems] = useState([]);
+    const [bubbleNewItems, setBubbleNewItems] = useState([]);
+
+    const getGenres = async () => {
+        try {
+            const data = await fetchAPI('/sliders', 'GET');
+
+            console.log(data);
+
+            const sliders = data.sliders;
+
+            const sortedGenres = Object.entries(sliders)
+                .map(([name, score]) => ({name, score}))
+                .sort((a, b) => b.score - a.score);
+
+            const topGenres = sortedGenres.slice(0, 3);
+            const nextGenres = sortedGenres.slice(3, 6);
+
+            setBubbleItems(topGenres);
+            setBubbleNewItems(nextGenres);
+
+
+        } catch (error) {
+            console.error("Error fetching genres:", error);
         }
-    ];
+    }
+
+    useEffect(() => {
+        getGenres();
+    }, []);
+
+    // const bubbleItems = [
+    //     {
+    //         "name": "EDM",
+    //         "listen_duration": 30
+    //     },
+    //     {
+    //         "name": "House",
+    //         "listen_duration": 25
+    //     },
+    //     {
+    //         "name": "Pop",
+    //         "listen_duration": 20
+    //     }
+    // ];
+    //
+    // const bubbleNewItems = [
+    //     {
+    //         "name": "Rap",
+    //         "listen_duration": 10
+    //     },
+    //     {
+    //         "name": "Trance",
+    //         "listen_duration": 5
+    //     },
+    //     {
+    //         "name": "DnB",
+    //         "listen_duration": 2
+    //     }
+    // ];
 
     return (
         <>
@@ -46,10 +81,10 @@ function Bubble() {
                         {bubbleItems.map((item, i) => {
                             const angle = (360 / bubbleItems.length) * i;
 
-                            const radius = (screen.width < 640) ? screen.width / 2 - 40 - 12 : 200;
+                            const radius = (screen.width < 640) ? screen.width / 2 - 40 - 20 : 200;
 
                             return (
-                                <div key={i} className={"absolute top-1/2 left-1/2 bg-body p-1"} style={{
+                                <div key={i} className={"absolute top-1/2 left-1/2 bg-body capitalize p-1"} style={{
                                     transform: `rotate(${angle}deg) translate(${radius}px) rotate(-${angle}deg) translate(-50%, -50%)`
                                 }}>
                                     {item.name}
@@ -74,7 +109,7 @@ function Bubble() {
 
                             return (
                                 <div key={i}
-                                     className={"absolute top-1/2 left-1/2 flex items-center bg-body aspect-square border border-secondary rounded-full p-2"}
+                                     className={"absolute top-1/2 left-1/2 flex items-center bg-body aspect-square border border-secondary rounded-full capitalize p-2"}
                                      style={{
                                          transform: `rotate(${angle}deg) translate(${radius}px) rotate(-${angle}deg) translate(-50%, -50%)`
                                      }}>
