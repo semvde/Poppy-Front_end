6269b962bd0b7682bf86deb051e9f90aa8f462bc
eline <1096534@hr.nl>
2026-03-18 10:47:31 +0100

    SHOW friendlist

--- src/pages/app/Friends.jsx
+++ src/pages/app/Friends.jsx
@@ -1,8 +1,69 @@
 import Button from "../../components/Button.jsx";
+import {fetchAPI} from "../../services/Fetch.js";
+import {useEffect, useState} from "react";
 
 function Friends() {
+
+    const [friends, setFriends] = useState([
+        {
+            "id": "user123",
+            "username": "john_doe",
+            "email": "john@example.com",
+            "image": "https://...",
+            "friendshipId": "9812364054123",
+            "since": "2026-03-10T12:00:00.000Z",
+            "status": "accepted"
+
+        },
+        {
+            "id": "user1234",
+            "username": "jane_doe",
+            "email": "jane@example.com",
+            "image": "https://...",
+            "friendshipId": "9812364054123",
+            "since": "2026-03-10T12:00:00.000Z",
+            "status": "accepted"
+
+        }
+    ])
+
+    const getFriendList = async () => {
+        try {
+            const data = await fetchAPI('/friends?userId={id}', 'GET');
+
+            console.log(data);
+            const friends = data.data;
+
+
+            setFriends(friends);
+
+
+        } catch (error) {
+            console.error("Error fetching friend requests:", error);
+        }
+    }
+
+    useEffect(() => {
+        // getFriendList();
+    }, []);
+
     return (
         <>
+
+            {/*searchbar*/}
+            <section className="py-10 space-y-5">
+
+                <div className="flex items-center gap-2 bg-body-light rounded-4xl px-4 py-2">
+                    <i className="fa-solid fa-magnifying-glass"></i>
+
+                    <input
+                        type="text"
+                        placeholder="Search friends..."
+                        className="w-full bg-transparent outline-none"
+                    />
+                </div>
+
+            </section>
 
             <div className="flex flex-wrap gap-2.5 p-5 justify-center text-center">
                 <h1>Friend Space</h1>
@@ -11,29 +72,33 @@ function Friends() {
                 </div>
             </div>
 
+
             <section className={"pb-10"}>
                 <div className={"flex flex-col gap-2.5"}>
-                    <Button variant={"secondary"}><i className={"mr-2.5 fa-solid fa-user-plus"}></i>Friend
+                    <Button as={"link"} to={"/app/friends/requests"} variant={"secondary"}><i
+                        className={"mr-2.5 fa-solid fa-user-plus"}></i>Friend
                         requests</Button>
                 </div>
             </section>
 
             <div className="p-5">
-                <h2>Friend list</h2>
+                <h2>Your friend list</h2>
             </div>
 
             <section className={"grid grid-cols-1 gap-2.5"}>
-                <Button as={"link"}>
-                    <div className="flex items-center justify-between w-full">
+                {friends.filter((friend) => friend.status === "accepted").map((friend) => (
+                    <Button key={friend.id} as={"link"}>
+                        <div className="flex items-center justify-between w-full">
 
-                        <span className="font-medium">@Username</span>
+                            <span className="font-medium">{friend.username}</span>
 
-                        <Button className={"px-2!"}>
-                            <i className="fa-solid fa-ellipsis-vertical"></i>
-                        </Button>
+                            <Button className={"px-2!"}>
+                                <i className="fa-solid fa-ellipsis-vertical"></i>
+                            </Button>
 
-                    </div>
-                </Button>
+                        </div>
+                    </Button>
+                ))}
             </section>
 
             {/*Voor als we de "You might know" gaan doen...*/}
